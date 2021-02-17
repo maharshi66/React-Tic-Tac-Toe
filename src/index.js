f9ed4afacf589126278cbb8cf225dae9d79f48c9
Maharshi Shah <40323681+maharshi66@y.github.com>
2021-02-17 15:41:55 +0530

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,62 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+class Square extends React.Component{
+	constructor(props){
+		super(props);
+		this.state =  {
+			value: null,
+		}
+	}
+
+	render(){	 
+		return (
+			<button className="square" onClick={() => this.setState({value: 'X'})}>
+				{this.state.value}
+			</button>
+		);
+	}
+}
+
+class Board extends React.Component{
+	renderSquare(i){
+		return <Square value={i} />;
+	}
+	
+	render() {
+	const status = "Next Player: X"; 
+		return (
+			<div>
+				<div className="status">{status}</div>
+				<div className="board-row">
+					{this.renderSquare(0)}
+					{this.renderSquare(1)}
+					{this.renderSquare(2)}
+				</div>
+				<div className="board-row">
+					{this.renderSquare(3)}
+					{this.renderSquare(4)}
+					{this.renderSquare(5)}
+				</div>
+				<div className="board-row">
+					{this.renderSquare(6)}
+					{this.renderSquare(7)}
+					{this.renderSquare(8)}
+				</div>	
+			</div>
+		)
+	}
+}
+
+class Game extends React.Component{
+	render() {
+		return <Board />;
+	}
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Game />,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
